b450774a313e57ffaa0756bd97e1417e54e9e732
kaylapark99 <kpark@colgate.edu>
2020-10-10 15:58:08 -0400

    fixing layout of thoughts

--- Tech/scriptsThoughts.js
+++ Tech/scriptsThoughts.js
@@ -1,6 +1,11 @@
 var hoverImg = document.querySelectorAll(".img-container-hover img");
 var clickImg = document.querySelectorAll(".img-container-click img");
 var close = document.getElementById("x");
+var captions = document.getElementById("instruction");
+var openTab = null;
+var currentCaption = null;
+var changeTime = document.getElementById("time");
+var coffeeClick = 0;
 
 function imgHover(x) {
     hoverImg[x].classList.remove("hide");
@@ -11,10 +16,28 @@ function imgLeave(x) {
 }
 
 function imgClick(x) {
+    openTab = x;
     clickImg[x].classList.remove("hide");
     close.classList.remove("hide");
+    currentCaption = captions.innerHTML;
+    if(x==0) {
+        captions.innerHTML = "test1";
+    }
 }
 
 close.addEventListener("click", function(){
-    
+    clickImg[openTab].classList.add("hide");
+    close.classList.add("hide");
+    captions.innerHTML = currentCaption;
 });
+
+document.getElementById("coffee").addEventListener("click", function(){
+    alert("hi");
+    currentCaption = captions.innerHTML;
+    captions.innerHTML = "do you really want to do that?";
+});
+
+
+setTimeout(function(){
+    changeTime.innerHTML = "it is 10:28 on a tuesday morning"
+}, 60000);
